edd8f0f7bfb33730771f86aa38d317708e020187
Lauren Fries <lauren.e.fries@hotmail.com>
2025-11-07 07:40:28 -0500

    lauren's 3 "boilerplate" classes

---
+++ app/models/scheduler.py
@@ -0,0 +1,20 @@
+class scheduler:
+  tasks: list[str] #PLACEHOLD FOR SCHEDULED TASKS
+
+  def __init__(self):
+    self.tasks = []
+  
+  def scheduleTask(self, task: str):
+    #complete this later
+    pass
+
+  def cancelTask(self, task: str):
+    #complete this later
+    pass
+
+  def executeTasks(self):
+    #complete this later
+    pass
+
+  def getScheduledTasks(self):
+    return self.tasks
